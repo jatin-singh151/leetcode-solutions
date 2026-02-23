ff39ed1fa7b0d95bb29671b8c34ad71910222dae
Jatin Singh <Jatin.28117@stu.upes.ac.in>
2026-02-23 09:29:29 +0530

    Implement function to detect cycle in linked list

---
+++ 141. Linked list Cycle.c
@@ -0,0 +1,10 @@
+bool hasCycle(struct ListNode *head) {
+    if(!head || !head->next) return false;
+    struct ListNode *ptr = head;
+    while(ptr->next){
+        if(ptr->val == INT_MAX) return true;
+        ptr->val = INT_MAX;
+        ptr = ptr->next;
+    }
+    return false;
+}
