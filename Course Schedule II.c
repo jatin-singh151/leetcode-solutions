9dfc4d9f234a31ddaed2994dea4c71c92a2a1211
Jatin Singh <Jatin.28117@stu.upes.ac.in>
2026-04-08 22:11:21 +0530

    Implement Course Schedule II using BFS

---
+++ Course Schedule II.c
@@ -0,0 +1,58 @@
+int* findOrder(int numCourses, int** prerequisites, int prerequisitesSize, int* prerequisitesColSize, int* returnSize) {
+    
+    int* indegree = (int*)calloc(numCourses, sizeof(int));
+    int** adj = (int**)malloc(numCourses * sizeof(int*));
+    int* adjSize = (int*)calloc(numCourses, sizeof(int));
+
+    // Initialize adjacency list
+    for (int i = 0; i < numCourses; i++) {
+        adj[i] = (int*)malloc(numCourses * sizeof(int));
+    }
+
+    // Build graph
+    for (int i = 0; i < prerequisitesSize; i++) {
+        int a = prerequisites[i][0];
+        int b = prerequisites[i][1];
+
+        adj[b][adjSize[b]++] = a;
+        indegree[a]++;
+    }
+
+    // Queue
+    int* queue = (int*)malloc(numCourses * sizeof(int));
+    int front = 0, rear = 0;
+
+    // Add nodes with indegree 0
+    for (int i = 0; i < numCourses; i++) {
+        if (indegree[i] == 0) {
+            queue[rear++] = i;
+        }
+    }
+
+    int* result = (int*)malloc(numCourses * sizeof(int));
+    int count = 0;
+
+    // BFS
+    while (front < rear) {
+        int node = queue[front++];
+        result[count++] = node;
+
+        for (int i = 0; i < adjSize[node]; i++) {
+            int neighbor = adj[node][i];
+            indegree[neighbor]--;
+
+            if (indegree[neighbor] == 0) {
+                queue[rear++] = neighbor;
+            }
+        }
+    }
+
+    // If cycle exists
+    if (count != numCourses) {
+        *returnSize = 0;
+        return (int*)malloc(0);
+    }
+
+    *returnSize = numCourses;
+    return result;
+}
